c99fd8c7f35a98239718621f10bbe729d83c903c
Patchett <rybridges16@gmail.com>
2016-05-15 15:47:52 -0700

    Obstacles now spawn and despawn and are properly animated on the platform.

---
+++ Assets/PhoneRunnerMiniGame/Scripts/Obstacle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Obstacle : MonoBehaviour {
+    private float box_width;
+    private float box_height;
+    public static float DESPAWN_POSX = -11.5f;
+    public static float CAMERA_LEFT_EDGE;
+    private SpawnObstacle controller;
+    // Use this for initialization
+    public void initialize(SpawnObstacle controller)
+    {
+        this.controller = controller;
+    }
+
+	void Start () {
+        CAMERA_LEFT_EDGE = Camera.main.transform.position.y - Camera.main.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (this.transform.position.x < -11.1)
+        {
+            this.remove();
+        }
+    }
+
+    void remove()
+    {
+        controller.removeObstacle(this);        
+    }
+}
---
+++ Assets/PhoneRunnerMiniGame/Scripts/SpawnObstacle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObstacle : MonoBehaviour {
+
+    public static float BOX_SPAWN_POSX = 11.49f;
+    public static float BOX_SPAWN_POSY = -2.56f;
+    private float last_spawn_time;
+    public Obstacle obstacle;
+	// Use this for initialization
+	void Start () {
+        last_spawn_time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float current_time = Time.time;
+        if(current_time - last_spawn_time > 1)
+        {
+            //height and width of camera - used to determine where to spawn food
+            float camHeight = Camera.main.orthographicSize;
+            float camWidth = camHeight * Camera.main.aspect;
+
+            //gets a random location relative to camera boundaries
+            Vector3 spawn_loc_vector = new Vector3(BOX_SPAWN_POSX, BOX_SPAWN_POSY);
+            Obstacle newObstacle = (Obstacle)GameObject.Instantiate(obstacle, spawn_loc_vector, new Quaternion());
+            newObstacle.initialize(this);
+            last_spawn_time = Time.time;
+        }
+    }
+
+    public void removeObstacle(Obstacle removeMe)
+    {
+        GameObject.Destroy(removeMe.gameObject);
+    }
+}

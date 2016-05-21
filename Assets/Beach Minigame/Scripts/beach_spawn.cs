9507e2364f90545e4a5f1b260c351d10d03addaf
Justin Chew <Justin Chew>
2016-05-21 13:27:06 -0700

    Added Beach Minigame init

---
+++ Assets/Beach Minigame/Scripts/beach_spawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beach_spawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] trash = (GameObject[])Resources.LoadAll<GameObject>("beach_prefab");
+
+        //spawn random small object
+        int ranInt = Random.Range(0, trash.Length);
+        GameObject sO = Instantiate(trash[ranInt]) as GameObject;
+        sO.transform.SetParent(GameObject.FindGameObjectWithTag("canvas").transform, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
---
+++ Assets/Beach Minigame/Scripts/trash_click.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trash_click : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnMouseDown() {
+        Debug.Log("Mouse Down!");
+        Destroy(this.gameObject);
+    }
+
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}

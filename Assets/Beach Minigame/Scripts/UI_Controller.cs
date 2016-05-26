9cfc7e9456cc30e13254986c54475c24706431cb
Betty <betty_louie1994@yahoo.com>
2016-05-25 17:59:36 -0700

    end scene stuff

---
+++ Assets/Beach Minigame/Scripts/UI_Controller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Controller : MonoBehaviour {
+    public GameObject GoButton;
+    public GameObject NextLevelPanel;
+    public GameObject WaveText;
+
+    public bool nextLevel;
+
+    // Use this for initialization
+    void Start () {
+        nextLevel = false;
+	}
+
+    public void updateButton() {
+        if(beach_spawn.numTrash == 0){
+            NextLevelPanel.SetActive(false);
+            GoButton.SetActive(false);
+            WaveText.SetActive(false);
+            nextLevel = true;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if(beach_spawn.numTrash == 0){
+            GoButton.SetActive(true);
+            NextLevelPanel.SetActive(true);
+        }
+        */
+        /*
+        else{
+            GoButton.SetActive(false);
+        }
+        */
+	}
+}

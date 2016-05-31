2d4b01bcb4f770be6b262c00b230bb1ff03bd685
Betty <betty_louie1994@yahoo.com>
2016-05-31 13:18:36 -0700

    beach start scene

--- Assets/Beach Minigame/Scripts/UI_Controller.cs
+++ Assets/Beach Minigame/Scripts/UI_Controller.cs
@@ -5,6 +5,15 @@ public class UI_Controller : MonoBehaviour {
     public GameObject GoButton;
     public GameObject NextLevelPanel;
     public GameObject WaveText;
+    public GameObject BeginButton;
+    public GameObject IntroBackground;
+    public GameObject WaveTutorial;
+    public GameObject TrashTutorial;
+    public GameObject AnimalTutorial;
+    public GameObject WaveArrow;
+    public GameObject TrashArrow;
+    public GameObject AnimalArrow;
+    public GameObject exTurtle, exFish, exTrash;
 
     public bool nextLevel;
 
@@ -13,13 +22,27 @@ public class UI_Controller : MonoBehaviour {
         nextLevel = false;
 	}
 
-    public void updateButton() {
+    public void updateEndLevelButton() {
         if(beach_spawn.numTrash == 0){
             NextLevelPanel.SetActive(false);
             GoButton.SetActive(false);
             WaveText.SetActive(false);
             nextLevel = true;
         }
+    }
+    public void updateStartGameButton()
+    {
+        BeginButton.SetActive(false);
+        IntroBackground.SetActive(false);
+        WaveTutorial.SetActive(false);
+        TrashTutorial.SetActive(false);
+        AnimalTutorial.SetActive(false);
+        WaveArrow.SetActive(false);
+        TrashArrow.SetActive(false);
+        AnimalArrow.SetActive(false);
+        exTurtle.SetActive(false);
+        exFish.SetActive(false);
+        exTrash.SetActive(false);
     }
 	
 	// Update is called once per frame

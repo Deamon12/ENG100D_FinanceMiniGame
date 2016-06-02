7768e8314df1970d4ddb5f50d311a72f6165e6c1
Justin Chew <Justin Chew>
2016-06-01 22:59:49 -0700

    Learn more button

--- Assets/beachMenuScript.cs
+++ Assets/beachMenuScript.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class beachMenuScript : MonoBehaviour {
-
+    private GameObject learnMore;
 	// Use this for initialization
 	void Start () {
-	
+        learnMore = GameObject.Find("Canvas/LearnMore");
+        learnMore.GetComponent<Canvas>().enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -17,12 +18,18 @@ public class beachMenuScript : MonoBehaviour {
     public void startBeachGame() {
         SceneManager.LoadScene("beach_start");
     }
-    
-    public void learnMoreButton() {
-        // Opens up learnMoreCanvas
-    }
 
     public void exitBeachGame() {
         SceneManager.LoadScene("OverviewMap");
+    }
+
+    public void learnMoreButton() {
+        // Opens up learnMoreCanvas
+        learnMore.GetComponent<Canvas>().enabled = true;
+    }
+
+    public void closeLearnMore() {
+        // Opens up learnMoreCanvas
+        learnMore.GetComponent<Canvas>().enabled = false;
     }
 }

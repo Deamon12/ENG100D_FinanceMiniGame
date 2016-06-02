c4f91bec7655570060cf19d76b18b01ed2ef91c0
Justin Chew <Justin Chew>
2016-06-01 22:42:36 -0700

    MainMenu

---
+++ Assets/beachMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class beachMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public void startBeachGame() {
+        SceneManager.LoadScene("beach_start");
+    }
+    
+    public void learnMoreButton() {
+        // Opens up learnMoreCanvas
+    }
+
+    public void exitBeachGame() {
+        SceneManager.LoadScene("OverviewMap");
+    }
+}

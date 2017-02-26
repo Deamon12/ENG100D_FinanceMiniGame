4960be5ce65fa7472d3364afaed47707c93e08db
Connor Smith <Connor Smith>
2017-02-26 15:50:37 -0800

    Fixed some compile errors

---
+++ Assets/Grocery Shopping Minigame/03-Scripts/GameOverControllerGrocery.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOverControllerGrocery : MonoBehaviour {
+
+    public static float point;
+    public static Text result;
+	// Use this for initialization
+	void Start () {
+        point = PointController.points;
+        result = GetComponent<Text>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("Inside game over controller");
+        result.text = "Congratulations! Your points is " + point + ". Thanks to you with all the money I saved, I can now afford a new dentist. ";
+    }
+
+}

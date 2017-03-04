41b9fd72d6e934c23aea44923657654d62393fcc
Deagan M <deamon1@hotmail.com>
2017-03-03 18:20:51 -0800

    Added land prefab, Added scripts to handle spawning and destroying land pieces.
    Added land prefab, Added scripts to handle spawning and destroying land
    pieces.

---
+++ Assets/KID$ - FinanceGame/Scripts/DestroyerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Enter");
+        Destroy(other.gameObject);
+
+    }
+    void OnTriggerStay(Collider other) { }
+    void OnTriggerExit(Collider other)
+    {
+        print("Trigger Exit");
+    }
+}
---
+++ Assets/KID$ - FinanceGame/Scripts/SpawnScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject[] spawnObj;
+    public float spawnTime = 1;
+
+	// Use this for initialization
+	void Start () {
+		 Spawn(13.7f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Spawn(float spacing)
+    {
+        Instantiate( spawnObj[0], new Vector3(transform.position.x + spacing, 0, 0), Quaternion.identity);// Random.Range (0, spawnObj.Length)]
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Enter");
+    }
+    void OnTriggerStay(Collider other) {}
+    void OnTriggerExit(Collider other)
+    {
+        print("Trigger Exit");
+        Spawn(24.5f);
+    }
+
+}
